7ea4545c33180b63a0234f40171ec9de8a05b231
keviveks <rajalingam299@gmail.com>
2018-07-18 23:23:45 +0530

    email added;

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -6,8 +6,8 @@ import{tokenNotExpired} from 'angular2-jwt'
 @Injectable()
 export class AuthService {
 
-  domain="http://localhost:8080";
-  //domain="";
+  //domain="http://localhost:8080";
+  domain:"";
   authToken;
   user;
   options;
